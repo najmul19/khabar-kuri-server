639e902cdb02a4ec98045aafd46084aa4c1d3a44
najmul <mdnajmulislam10992@gmail.com>
2025-10-05 21:20:27 +0600

    fixed the backend menu update id string to object

--- index.js
+++ index.js
@@ -160,10 +160,37 @@ async function run() {
       const result = await menuCollection.find().toArray();
       res.send(result);
     });
+    // app.patch("/menu/:id", async (req, res) => {
+    //   const item = req.body;
+    //   const id = req.params.id;
+    //   const query = { _id: id };
+    //   const updatedDoc = {
+    //     $set: {
+    //       name: item.name,
+    //       category: item.category,
+    //       price: item.price,
+    //       recipe: item.recipe,
+    //       image: item.image,
+    //     },
+    //   };
+    //   const result = await menuCollection.updateOne(query, updatedDoc);
+    //   res.send(result);
+    // });
+    // app.get("/menu/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) };
+    //   const result = await menuCollection.findOne(query);
+    //   res.send(result);
+    // });
+   
+
     app.patch("/menu/:id", async (req, res) => {
       const item = req.body;
       const id = req.params.id;
-      const query = { _id: id };
+
+      // convert string to ObjectId
+      const query = { _id: new ObjectId(id) };
+
       const updatedDoc = {
         $set: {
           name: item.name,
@@ -173,15 +200,11 @@ async function run() {
           image: item.image,
         },
       };
+
       const result = await menuCollection.updateOne(query, updatedDoc);
       res.send(result);
     });
-    // app.get("/menu/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await menuCollection.findOne(query);
-    //   res.send(result);
-    // });
+
     app.get("/menu/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: id };
