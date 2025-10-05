d062275d340783201b1f5b87fd7e690aa8c5db2e
najmul <mdnajmulislam10992@gmail.com>
2025-10-05 21:45:45 +0600

    fixed bug on get menu

--- index.js
+++ index.js
@@ -207,7 +207,7 @@ async function run() {
 
     app.get("/menu/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: id };
+      const query = {_id: new ObjectId(id) };
       const result = await menuCollection.findOne(query);
       res.send(result);
     });
